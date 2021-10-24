8f0428e6cb4b8ab11f5b8ff1b8a548f47623d5b6
胡昊 <980850497@qq.com>
2021-10-24 19:59:19 +0800

    update the code which cope the dataset

--- stick_barcode/Semantic_Segmentation/dateset/cope_voc_dateset.py
+++ stick_barcode/Semantic_Segmentation/dateset/cope_voc_dateset.py
@@ -25,7 +25,9 @@ def copy_xml_to_other_folder(pic_path,xml_path):
 
 def xml_to_txt(xml_path, outdir):
     """读取xml文件，获得想要的内容存到txt文件中"""
+    """XML设计的核心是包含和传输数据"""
     xml_list = os.listdir(xml_path)
+
     for xml_file in xml_list:
         xml_file_path = osp.join(xml_path,xml_file)
         if '.DS_Store' in xml_file_path:
@@ -33,61 +35,18 @@ def xml_to_txt(xml_path, outdir):
         else:
             DOMTree = ET.parse(xml_file_path)
             root = DOMTree.getroot()
-            for bndbox in root.iter('bndbox'):
-                node = []
-                for child in bndbox:
-                    node.append(int(child.text))
-                x1, y1 = node[0], node[1]
-                x3, y3 = node[2], node[3]
-                print(x1,y1,x3,y3)
-
-    # for i, file in enumerate(xml_path):
-    #     file_save = file.split('.')[0] + '.txt'
-    #     file_txt = os.path.join(outdir, file_save)
-    #     f_w = open(file_txt, 'w')
-    #     # actual parsing
-    #     DOMTree = xml.dom.minidom.parse(file)
-    #     annotation = DOMTree.documentElement
-    #     filename = annotation.getElementsByTagName("path")[0]
-    #     imgname = filename.childNodes[0].data
-    #     img_temp = imgname.split('\\')[-1]
-    #     img_temp = os.path.join(image_dir, img_temp)
-    #     image = cv.imread(imgname)
-    #     cv.imwrite(img_temp, image)
-    #     objects = annotation.getElementsByTagName("object")
-    #     print(file)
-    #     for object in objects:
-    #         bbox = object.getElementsByTagName("robndbox")[0]
-    #         cx = bbox.getElementsByTagName("cx")[0]
-    #         x = float(cx.childNodes[0].data)
-    #         print(x)
-    #         cy = bbox.getElementsByTagName("cy")[0]
-    #         y = float(cy.childNodes[0].data)
-    #         print(y)
-    #         cw = bbox.getElementsByTagName("w")[0]
-    #         w = float(cw.childNodes[0].data)
-    #         print(w)
-    #         ch = bbox.getElementsByTagName("h")[0]
-    #         h = float(ch.childNodes[0].data)
-    #         print(h)
-    #         cangel = bbox.getElementsByTagName("angle")[0]
-    #         angle = float(cangel.childNodes[0].data)
-    #         print(angle)
-    #         cname = object.getElementsByTagName("name")[0]
-    #         name = cname.childNodes[0].data
-    #         print(name)
-    #         x1 = x - w / 2.
-    #         y1 = y - h / 2.
-    #         x2 = x + w / 2.
-    #         y2 = y - h / 2.
-    #         x3 = x + w / 2.
-    #         y3 = y + h / 2.
-    #         x4 = x - w / 2.
-    #         y4 = y + h / 2.
-    #         temp = str(x1) + ' ' + str(y1) + ' ' + str(x2) + ' ' + str(y2) + ' ' + str(x3) + ' ' + str(y3) + ' ' + str(
-    #             x4) + ' ' + str(y4) + ' ' + name + '\n'
-    #         f_w.write(temp)
-    #     f_w.close()
+            for object in root.iter('object'):
+                object_bndbox = object.iter('bndbox')
+                object_name = object.iter('name')
+                for bndbox in object_bndbox:
+                    node = []
+                    for child in bndbox:
+                        node.append(int(child.text))
+                    x, y = node[0], node[1]
+                    w, h = node[2], node[3]
+                for name in object_name:
+                    name = name.text
+                print(x,y,w,h,name)
 
 
 def get_new_name(pic_path):
